cafb3f641971904050c8560d9ba3a37e80c922eb
Rowland <rowland.wanderi@student.moringaschool.com>
2023-10-05 05:45:28 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+
+function reverseString(word) {
+  // Step 1. Use the split() method to return a new array
+  let splitString = word.split(""); // let splitString = "hello".split("");
+  // ["h", "e", "l", "l", "o"]
+
+  // Step 2. Use the reverse() method to reverse the new created array
+  let reverseArray = splitString.reverse(); // let reverseArray = ["h", "e", "l", "l", "o"].reverse();
+  // ["o", "l", "l", "e", "h"]
+
+  // Step 3. Use the join() method to join all elements of the array into a string
+  let joinArray = reverseArray.join(""); // let joinArray = ["o", "l", "l", "e", "h"].join("");
+  // "olleh"
+  
+  //Step 4. Return the reversed string
+  return joinArray
 }
 
+function isPalindrome(word){
+  const reversedWord = reverseString(word)
+  return word === reversedWord
+}
 /* 
   Add your pseudocode here
+
+  reverse the input string
+
+if the reversed string is the same as the input
+  return true
+else
+  return false
 */
 
 /*
@@ -18,8 +43,45 @@ if (require.main === module) {
 
   console.log("");
 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
+
+
+/* another method without using in built methods
+
+function isPalindrome(word) {
+  // Write your algorithm here
+  // iterate from the beginning of the string to the middle of the string
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    // compare the letter we're iterating over to the corresponding letter at the end of the string
+    let endIndex = word.length - 1 - startIndex;
+    // if the letters don't match, return false
+    if (word[startIndex] !== word[endIndex]) return false;
+  }
+  return true;
+}
+
+*/
